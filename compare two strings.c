09c6ffd1fd9d55bf7e495b25f36f117c8d3bd7f1
Noble <78396843+Noblechakma@users.noreply.github.com>
2021-02-01 20:17:19 -0800

    Update compare two strings.c

--- compare two strings.c
+++ compare two strings.c
@@ -4,7 +4,7 @@ int main()
     int i ;
     char a[50],b[50];
     scanf("%[^\n] %[^\n]",a,b);
-    for(i = 0;a[i]==b[i] && a[i]!=0 && b[i]!=0; i++);
+    for(i = 0;a[i] == b[i] && a[i]!='\0'&& b[i]!='\0'; i++);
     if(a[i]>b[i])
     {
         printf("The  first string is greater");
